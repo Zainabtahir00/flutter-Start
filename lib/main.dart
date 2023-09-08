819daf4f95cb28fa2c55ba4e4cd7e4d579308335
Zainabtahir00 <zainabt025@gmial.com>
2023-09-08 04:07:44 -0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Pakistan Cricket Team"),
-          backgroundColor: Colors.green,
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Pakistan'),
+            backgroundColor: Colors.green,
+          ),
+          body: Center(
+            child: Container(
+              alignment: Alignment.center,
+              color: Colors.green,
+              height: 100.0,
+              width: 150.0, // Set the background color here
+              child: Text('Worldcup!!!!!'),
+            ),
+          ),
         ),
-        body: Center(child: Image(image: AssetImage('images/pAK.png'))),
       ),
-    ));
+    );
+  }
+}
